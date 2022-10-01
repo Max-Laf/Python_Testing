d38ee754ec811fefb4847e9a4a8868974375349d
Maxence Lafond <maxence.lafond.1@ens.etsmtl.ca>
2022-09-30 21:38:02 -0400

    TestingStyles
    Ajout mystylesheets pour savoir comment styler

--- PythonTesting1.py
+++ PythonTesting1.py
@@ -1,35 +1,62 @@
+from cgitb import text
+
 import sys
-
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QMessageBox
 
 
-class MainWindow(QWidget):
+class MyApp(QWidget):
+
     def __init__(self):
         super().__init__()
-        outerLayout = QVBoxLayout()
-        toplayout = QHBoxLayout()
-        lowerLayout = QHBoxLayout()
 
-        toplayout.addWidget(QLabel("RPM  "), 1)
-        toplayout.addWidget(QLabel("Speed"), 2)
-        toplayout.addWidget(QLabel("Throttle"), 3)
-        toplayout.addStretch()
+        self.initUI()
 
-        lowerLayout.addWidget(QPushButton("ADD"), 0)
-        lowerLayout.addWidget(QPushButton("Lower"), 1)
-        lowerLayout.addWidget(QPushButton("Manage"), 2)
-        lowerLayout.addStretch()
+    def initUI(self):
 
-        outerLayout.addLayout(toplayout)
-        outerLayout.addLayout(lowerLayout)
+        lbl_red = QLabel('RPM')
+        lbl_green = QLabel('Throttle')
+        lbl_blue = QLabel('SPeed')
 
-        self.setLayout(outerLayout)
-        self.setGeometry(100, 100, 200, 100)
-        self.setWindowTitle("Push for window")
+        lbl_red.setStyleSheet(open('mystylesheet.css').read())
+        lbl_green.setStyleSheet(open('mystylesheet.css').read())
+        lbl_blue.setStyleSheet(open('mystylesheet.css').read())
+
+        button1 = QPushButton("Show Text")
+        self.textbox1 = QLineEdit(self)
+        self.textbox1.move(20, 20)
+        self.textbox1.resize(280, 40)
+
+        button1.setStyleSheet(open('mystylesheet.css').read())
+        self.textbox1.setStyleSheet(open('mystylesheet.css').read())
+
+        outerbox = QHBoxLayout()
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl_red)
+        vbox.addWidget(lbl_green)
+        vbox.addWidget(lbl_blue)
+
+        vbox2 = QVBoxLayout()
+        vbox2.addWidget(self.textbox1)
+        vbox2.addWidget(button1)
+
+        outerbox.addLayout(vbox, 0)
+        outerbox.addLayout(vbox2, 1)
+        outerbox.addStretch()
+
+        self.setLayout(outerbox)
+
+        self.setWindowTitle('Programme')
+        #self.setGeometry(150, 150, 1500, 800)
+        button1.clicked.connect(self.on_click)
+        self.show()
+
+    def on_click(self):
+        print(self.textbox1.text())
+        self.textbox1.setText("")
 
 
 app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
-app.exec()
+app.setStyleSheet(open('mystylesheet.css').read())
+ex = MyApp()
+sys.exit(app.exec_())
